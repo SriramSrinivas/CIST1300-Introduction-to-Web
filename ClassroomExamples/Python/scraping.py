f2d3249ac176764c9ee32a0176439fce46ae632e
Sriram Srinivasa <sriramsrinivas@unomaha.edu>
2019-01-04 03:32:00 +0000

    chnages

---
+++ ClassroomExamples/Python/scraping.py
@@ -0,0 +1,19 @@
+import urllib
+from bs4 import BeautifulSoup
+
+# Fetch the html file
+
+response = urllib.request.urlopen('https://www.amazon.com/dp/B0792KTHKJ/ref=ods_gw_d_cmdw_dnut_aucc_1127?pf_rd_p=984825d7-7af6-44a5-953c-1d5e3c0af1f5&pf_rd_r=N7S1PKSXRX94TFDMR7XH')
+html_doc = response.read()
+# print(html_doc)
+
+# Parse the html file
+soup = BeautifulSoup(html_doc, "html.parser")
+
+# print(soup)
+# Format the parsed html file
+strhtm = soup.prettify()
+
+# Print the first few characters
+# print (strhtm[:225])
+for x in soup.find("span",{"id":"priceblock_dealprice"}): print(x.string)
