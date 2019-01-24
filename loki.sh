dd9b5ec914f6a8719f4bf11c02f8b5e333d74343
Sriram Srinivasan <sriramsrinivas@unomaha.edu>
2019-01-23 20:31:07 -0600

    Loki Script

---
+++ loki.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+USERNAME=sriramsrinivas
+HOSTS="loki.ist.unomaha.edu"
+SCRIPT="cd public_html;cd CIST1300-001;ls;git pull; publish-public-html"
+for HOSTNAME in ${HOSTS} ; do
+    ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+done
