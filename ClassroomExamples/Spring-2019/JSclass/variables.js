6bb58bb361eb8b969669004ebd71a820a42dffda
Sriram Srinivasan <sriramsrinivas@unomaha.edu>
2019-03-26 23:52:08 -0500

    Chnages

---
+++ ClassroomExamples/Spring-2019/JSclass/variables.js
@@ -0,0 +1,62 @@
+var number1=1;
+var letter="ABCDE";
+var testIndex=5;
+var number2=3;
+var sum=0;
+var product=1;
+var division=0;
+var subtract=0;
+var number3=5;
+var number4=6;
+// console.log(number1,number2, sum,testIndex,letter);
+
+function addition( number3, number4)
+{
+
+    sum=number3+number4;
+    return sum;
+
+}
+
+
+function multiplication(number1,number2)
+
+{
+    product=number1*number2;
+    return product
+
+
+
+}
+
+
+multiplication();
+
+// console.log("After multiplication", product);
+
+
+
+ var value=addition(number3, number4);
+
+// console.log("After Addition", sum);
+
+function divide(number1,number2) {
+
+    division=number1/number2;
+    return division;
+}
+divide();
+
+// console.log("After Division", division);
+
+
+
+function subtraction(number1,number2) {
+
+    subtract=number2-number1;
+    return subtract;
+
+}
+subtraction();
+
+// console.log("After Subtraction", subtract);
